e52e0b306a7ae6aaabd525734582b31cc4d0fdea
Ak <Akshat76845>
2025-07-04 16:48:18 +0530

    Removed dist folder

--- brainly/dist/db.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UserModel = void 0;
-const mongoose_1 = require("mongoose");
-const UserSchema = new mongoose_1.Schema({
-    username: { type: String, unique: true },
-    password: String
-});
-exports.UserModel = new mongoose_1.Model("User", UserSchema);
